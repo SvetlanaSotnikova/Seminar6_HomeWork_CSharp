b555088bc5ba6fab336acbf2a0fffc794ddc660b
Svetik <sotnikovas337@gmail.com>
2023-08-23 22:17:08 +0300

    sa

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+/*
+using System.Runtime.Intrinsics.X86;
+
+int[] CreateArr(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input element {i + 1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return arr;
+}
+
+void WriteArr(int[] arr)
+{
+    string strArr = string.Join(", ", arr);
+    Console.Write($"{strArr} -> ");
+}
+
+int GetCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if ((arr[i]) > 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Write("Input a length of array: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArr(m);
+WriteArr(arr);
+Console.WriteLine($"{GetCount(arr)}");
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void GetPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+Console.Write("Input value b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input value k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input value b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input value k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+GetPoint(b1,k1,b2,k2);
+
