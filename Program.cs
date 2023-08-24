67a0874b2f4fa60e18d039bb1e3588dcbdbaff12
Svetik <sotnikovas337@gmail.com>
2023-08-24 13:53:29 +0300

    update

--- Program.cs
+++ Program.cs
@@ -46,7 +46,7 @@ Console.WriteLine($"{GetCount(arr)}");
 //  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
+/*
 void GetPoint(double b1, double k1, double b2, double k2)
 {
     double x = (b2 - b1) / (k1 - k2);
@@ -66,4 +66,37 @@ Console.Write("Input value k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
 GetPoint(b1,k1,b2,k2);
+*/
 
+// Написать программу, преобразующую число из десятеричной системы счисления в двоичную.
+
+int[] DecimalToBinary(int number)
+{
+    if (number == 0) return new int[] { 0 };
+
+    int[] arr = new int[32];
+    int i = 0;
+    while (number > 0)
+    {
+        arr[i] = number % 2;
+        number /= 2;
+        i++;
+    }
+    Array.Resize(ref arr, i); // уменьшение размера массива до его реальной длины
+    return arr;
+}
+Console.Write("Enter a number in decimal: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = DecimalToBinary(num);
+Console.Write($"number {num} in binary: ");
+for (int i=0;i<arr.Length;i++)
+{
+    Console.Write(arr[i]);
+}
+Console.WriteLine();
+
+// Намного упращенный вариант
+Console.Write("Enter a number in decimal: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string binNum = Convert.ToString(num, 2);
+Console.Write($"number {number} in binary: {binNum} ");
